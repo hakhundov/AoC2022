41d5964150bb0ad7995df0edf3ea6b6de98df471
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-03 20:34:41 +0100

    add a test for part 1

--- Day3/test_rucksack.py
+++ Day3/test_rucksack.py
@@ -1,4 +1,4 @@
-from rucksack import getPriority
+from rucksack import getPriority, solvePart1
 
 
 def test_getPriority():
@@ -6,3 +6,6 @@ def test_getPriority():
     assert getPriority('z') == 26
     assert getPriority('A') == 27
     assert getPriority('Z') == 52
+
+def test_partOne():
+    assert solvePart1("input") == 7917
