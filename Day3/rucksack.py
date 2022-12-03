e2156b82ae12037c7cd51575278cdbccf28dfc25
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-03 19:07:42 +0100

    day 3 part 2

--- Day3/rucksack.py
+++ Day3/rucksack.py
@@ -1,5 +1,7 @@
 # AoC 2022 - Day 3 - Rucksack Reorganization
 
+# Part 1
+
 file = open("input", "r")
 sumOfPriorities = 0
 
@@ -11,4 +13,17 @@ for line in file:
 
 print(sumOfPriorities)
 if (sumOfPriorities == 7917):
+    print('PASSED!')
+
+# Part 2
+
+file.seek(0)
+sumOfPriorities = 0
+
+for line in file:
+    commonItem = ''.join( set(line.strip()) & set(next(file).strip()) & set(next(file).strip()) )
+    sumOfPriorities += (ord(commonItem) - 96) if commonItem.islower() else (ord(commonItem) - 38)
+
+print(sumOfPriorities)
+if (sumOfPriorities == 2585):
     print('PASSED!')
