5bbdf765547e96c3f4203c608be5e92b06b51546
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-03 20:22:29 +0100

    added the getPriority function

--- Day3/rucksack.py
+++ Day3/rucksack.py
@@ -5,13 +5,17 @@
 file = open("input", "r")
 sumOfPriorities = 0
 
+def getPriority(item):
+    return (ord(item) - 96) if item.islower() else (ord(item) - 38)
+
 for line in file:
     firstCompartment = set(line[:len(line)//2].strip())
     secondCompartment = set(line[len(line)//2:].strip())    
     sharedItem = ''.join(firstCompartment.intersection(secondCompartment))
-    sumOfPriorities += (ord(sharedItem) - 96) if sharedItem.islower() else (ord(sharedItem) - 38)
+    sumOfPriorities += getPriority(sharedItem)
 
 print(sumOfPriorities)
+
 if (sumOfPriorities == 7917):
     print('PASSED!')
 
@@ -25,5 +29,6 @@ for line in file:
     sumOfPriorities += (ord(commonItem) - 96) if commonItem.islower() else (ord(commonItem) - 38)
 
 print(sumOfPriorities)
+
 if (sumOfPriorities == 2585):
     print('PASSED!')
