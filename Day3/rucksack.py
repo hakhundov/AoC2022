bab362090068cfa2e060c66bc3543cc7ab08e044
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-03 20:50:30 +0100

    using With open as file - removing explicit file close

--- Day3/rucksack.py
+++ Day3/rucksack.py
@@ -5,28 +5,26 @@ def getPriority(item):
     return (ord(item) - 96) if item.islower() else (ord(item) - 38)
 
 def solvePart1(inputFilename):
-    file = open(inputFilename, "r")
     sumOfPriorities = 0
 
-    for line in file:
-        firstCompartment = set(line[:len(line)//2].strip())
-        secondCompartment = set(line[len(line)//2:].strip())
-        sharedItem = ''.join(firstCompartment.intersection(secondCompartment))
-        sumOfPriorities += getPriority(sharedItem)
-    file.close()
+    with open(inputFilename, "r") as file:
+        for line in file:
+            firstCompartment = set(line[:len(line)//2].strip())
+            secondCompartment = set(line[len(line)//2:].strip())
+            sharedItem = ''.join(firstCompartment.intersection(secondCompartment))
+            sumOfPriorities += getPriority(sharedItem)
+
     return sumOfPriorities
 
 # Part 2
 def solvePart2(inputFilename):
-    file = open(inputFilename, "r")
     sumOfPriorities = 0
 
-    for line in file:
-        commonItem = ''.join( set(line.strip()) & set(next(file).strip()) & set(next(file).strip()) )
-        sumOfPriorities += (ord(commonItem) - 96) if commonItem.islower() else (ord(commonItem) - 38)
-    file.close()
+    with open(inputFilename, "r") as file:
+        for line in file:
+            commonItem = ''.join( set(line.strip()) & set(next(file).strip()) & set(next(file).strip()) )
+            sumOfPriorities += getPriority(commonItem)
     return sumOfPriorities
-
 
 print("Part 1 answer: " + str(solvePart1("input")))
 print("Part 2 answer: " + str(solvePart2("input")))
