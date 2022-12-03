9ebd10a6682ded6f09a8b106fcf86dea33872ebd
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-03 20:35:23 +0100

    refactoring in progress

--- Day3/rucksack.py
+++ Day3/rucksack.py
@@ -2,33 +2,32 @@
 
 # Part 1
 
-file = open("input", "r")
-sumOfPriorities = 0
-
 def getPriority(item):
     return (ord(item) - 96) if item.islower() else (ord(item) - 38)
 
-for line in file:
-    firstCompartment = set(line[:len(line)//2].strip())
-    secondCompartment = set(line[len(line)//2:].strip())    
-    sharedItem = ''.join(firstCompartment.intersection(secondCompartment))
-    sumOfPriorities += getPriority(sharedItem)
+def solvePart1(inputFilename):
+    file = open(inputFilename, "r")
+    sumOfPriorities = 0
 
-print(sumOfPriorities)
+    for line in file:
+        firstCompartment = set(line[:len(line)//2].strip())
+        secondCompartment = set(line[len(line)//2:].strip())
+        sharedItem = ''.join(firstCompartment.intersection(secondCompartment))
+        sumOfPriorities += getPriority(sharedItem)
 
-if (sumOfPriorities == 7917):
-    print('PASSED!')
+    return sumOfPriorities
 
 # Part 2
+def solvePart2():
+    file.seek(0)
+    sumOfPriorities = 0
 
-file.seek(0)
-sumOfPriorities = 0
+    for line in file:
+        commonItem = ''.join( set(line.strip()) & set(next(file).strip()) & set(next(file).strip()) )
+        sumOfPriorities += (ord(commonItem) - 96) if commonItem.islower() else (ord(commonItem) - 38)
+    return sumOfPriorities
 
-for line in file:
-    commonItem = ''.join( set(line.strip()) & set(next(file).strip()) & set(next(file).strip()) )
-    sumOfPriorities += (ord(commonItem) - 96) if commonItem.islower() else (ord(commonItem) - 38)
+# print(sumOfPriorities)
 
-print(sumOfPriorities)
-
-if (sumOfPriorities == 2585):
-    print('PASSED!')
+# if (sumOfPriorities == 2585):
+#     print('PASSED!')
