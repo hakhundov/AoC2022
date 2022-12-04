4c44d959fa816aa761c4c042f8bedb117845b625
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-04 18:36:33 +0100

    add tests + test input

---
+++ Day2/test_RockPaperScissors.py
@@ -0,0 +1,9 @@
+from RockPaperScissors import solveDay2
+
+
+def test_realData():
+    assert solveDay2("input") == [14163, 12091]
+
+
+def test_testInput():
+    assert solveDay2("testInput") == [15, 12]
