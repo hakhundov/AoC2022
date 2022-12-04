0972667cf140af7c7b3d96e4aea1458edf57b630
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-04 09:26:44 +0100

    combined two functions for both parts and fixed test

--- Day4/campCleanup.py
+++ Day4/campCleanup.py
@@ -36,3 +36,24 @@ def solvePart2(inputFilename):
     return count - noOverlaps + 1
 
 print(solvePart2("input"))
+
+
+
+def solveDay4(inputFilename):
+    fullOverlaps = 0 #subset
+    noOverlaps = 0   #disjoint
+
+    with open(inputFilename, "r") as file:
+        for count, line in enumerate(file):
+            sections = [x.split('-') for x in line.strip().split(',')]
+            ranges = [int(val) for sublist in sections for val in sublist]
+            range1 = set(range(ranges[0], ranges[1] + 1))
+            range2 = set(range(ranges[2], ranges[3] + 1))
+            if not range1 - range2 or not range2 - range1:
+                fullOverlaps += 1
+            if not (range1 & range2):
+                noOverlaps += 1
+    return [fullOverlaps, count - noOverlaps + 1]
+
+
+print(solveDay4("input"))
--- Day4/test_campCleanup.py
+++ Day4/test_campCleanup.py
@@ -1,4 +1,4 @@
-from campCleanup import solvePart1, solvePart2
+from campCleanup import solvePart1, solvePart2, solveDay4
 
 def test_partOne():
     assert solvePart1("testInput") == 2
@@ -6,4 +6,8 @@ def test_partOne():
 
 def test_partTwo():
     assert solvePart2("testInput") == 4
-    # assert solvePart1("input") ==
+    assert solvePart2("input") == 870
+
+def test_day4():
+    assert solveDay4("testInput") == [2,4]
+    assert solveDay4("input") == [509,870]
