c9f10dac04fe3703e68075601c34bc60c5a085eb
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-04 09:29:17 +0100

    cleanup - remove old functions and test

--- Day4/campCleanup.py
+++ Day4/campCleanup.py
@@ -1,47 +1,8 @@
 # AoC 2022 - Day 4 - https://adventofcode.com/2022/day/4
 
-# Part 1
-
-def solvePart1(inputFilename):
-    overlaps = 0
-    with open(inputFilename, "r") as file:
-        for line in file:
-            sections = [x.split('-') for x in line.strip().split(',')]
-            ranges = [int(val) for sublist in sections for val in sublist]
-            range1 = set(range(ranges[0], ranges[1] + 1))
-            range2 = set(range(ranges[2], ranges[3] + 1))
-
-            if not range1 - range2 or not range2 - range1:
-                overlaps += 1
-    return overlaps
-
-print(solvePart1("input"))
-
-
-# Part 2
-
-def solvePart2(inputFilename):
-    noOverlaps = 0
-    with open(inputFilename, "r") as file:
-        for count, line in enumerate(file):
-            sections = [x.split('-') for x in line.strip().split(',')]
-            ranges = [int(val) for sublist in sections for val in sublist]
-            range1 = set(range(ranges[0], ranges[1] + 1))
-            range2 = set(range(ranges[2], ranges[3] + 1))
-            
-
-            if not (range1 & range2):
-                noOverlaps += 1
-
-    return count - noOverlaps + 1
-
-print(solvePart2("input"))
-
-
-
 def solveDay4(inputFilename):
-    fullOverlaps = 0 #subset
-    noOverlaps = 0   #disjoint
+    fullOverlaps = 0  # subset
+    noOverlaps = 0  # disjoint
 
     with open(inputFilename, "r") as file:
         for count, line in enumerate(file):
--- Day4/test_campCleanup.py
+++ Day4/test_campCleanup.py
@@ -1,13 +1,6 @@
-from campCleanup import solvePart1, solvePart2, solveDay4
+from campCleanup import solveDay4
 
-def test_partOne():
-    assert solvePart1("testInput") == 2
-    assert solvePart1("input") == 509
-
-def test_partTwo():
-    assert solvePart2("testInput") == 4
-    assert solvePart2("input") == 870
 
 def test_day4():
-    assert solveDay4("testInput") == [2,4]
-    assert solveDay4("input") == [509,870]
+    assert solveDay4("testInput") == [2, 4]
+    assert solveDay4("input") == [509, 870]
