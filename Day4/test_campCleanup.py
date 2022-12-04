e4e6c001fd08ec1e85ec22754bf21337c9f9d9de
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-04 09:04:29 +0100

    add test for d2/p2 with testinput

--- Day4/test_campCleanup.py
+++ Day4/test_campCleanup.py
@@ -1,5 +1,9 @@
-from campCleanup import solvePart1
+from campCleanup import solvePart1, solvePart2
 
 def test_partOne():
     assert solvePart1("testInput") == 2
     assert solvePart1("input") == 509
+
+def test_partTwo():
+    assert solvePart1("testInput") == 4
+    # assert solvePart1("input") == 509
