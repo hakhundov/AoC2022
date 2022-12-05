09018d08c92f036d5471015a7e57c1dbef29802b
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-06 00:29:14 +0100

    intermidiate state - reading data from file now propertly for tests

--- Day5/SupplyStacks.py
+++ Day5/SupplyStacks.py
@@ -4,27 +4,63 @@ from aocd import get_data
 from aocd import submit
 import re
 
-data = get_data(day=5, year=2022).splitlines()
+# data = get_data(day=5, year=2022).splitlines()
+
+data = []
+with open("testInput", "r") as file:
+    for line in file:
+        data.append(''.join(line.splitlines()))
+
+# part 1
+print(data)
 
 for count, line in enumerate(data):
     if line == '':
         break
 
 totalStacks = re.findall(r"\d+(?!\d+)", data[count-1])[-1]
-allStacks = [[] for i in range(count)]   
+print(totalStacks)
+allStacks = [[] for i in range(int(totalStacks))]   
 
 for x in range(count-2, -1, -1): #upside down stack
     cursor = 1
     for stack in range(int(totalStacks)): # counting from 0
-        item = data[x][cursor]
-        if item != ' ':
-            allStacks[stack].append(item)
-        cursor += 4
+        try:
+            item = data[x][cursor]
+            if item != ' ':
+                allStacks[stack].append(item)
+            cursor += 4
+        except IndexError:
+            break;
+        
+        print(cursor)
+        print(allStacks)
+
+print("-------")
+
 
 for line in range(count+1, len(data), 1):
     matches = [int(n) for n in re.findall(r"-?\d+", data[line])]
+    # part 1
     for _ in range(matches[0]):
         allStacks[matches[2]-1].append(allStacks[matches[1]-1].pop()) # indexing starts at 0, hence -1
+    
+    # part 2 - no rearrangements - can pick up multiple cargos
+    # intermidiate = []
+    # for _ in range(matches[0]):
+    #     #remove from
+    #     intermidiate.append(allStacks[matches[1]-1].pop())
+    # intermidiate.reverse()
+    # for _ in range(matches[0]):
+    #     #add to
+    #     allStacks[matches[2]-1].append(intermidiate.pop())
 
+        # allStacks[matches[1]-1] = allStacks[matches[1]-1][0: matches[0] * (-1)] 
+        # allStacks[matches[2]-1].extend(allStacks[matches[1]-1][: len(allStacks[matches[1]-1]) - matches[0]] )
+
+# print(allStacks)
 for x in range(len(allStacks)):
     print(allStacks[x][-1], end="")
+
+
+# part 2
--- Day5/test_SupplyStacks.py
+++ Day5/test_SupplyStacks.py
@@ -1,6 +1,13 @@
 # from SupplyStacks import *
 
+# get test input data from file
+data = []
+with open("testInput", "r") as file:
+    for line in file:
+        data.append(line.strip())
 
+
+# print(data)
 # def test_day4():
 #     assert solveDay4P1("testInput") ==
 #     assert solveDay4P1("input") == "FRDSQRRCD"
