614df6123f86de98fb6fd25977ab2826793bf8de
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-06 19:04:12 +0100

    day 6 solution

---
+++ Day6/tuningTrouble.py
@@ -0,0 +1,27 @@
+# AoC 2022 - Day 6 - https://adventofcode.com/2022/day/6
+
+from aocd import get_data
+from aocd import submit
+
+data = get_data(day=6, year=2022).splitlines()
+
+
+def checkRepetition(inputPattern):
+    if len(set(inputPattern)) != len(inputPattern):
+        return True
+    return False
+
+
+def solve(data, markerLength):
+    pattern = [x for x in data[0][:markerLength]]
+
+    for index in range(markerLength, len(data[0])+1, 1):
+        if (checkRepetition(pattern)):
+            pattern = pattern[1:] + [data[0][index]]
+        else:
+            break
+    return index
+
+
+print(solve(data, 4))
+print(solve(data, 14))
