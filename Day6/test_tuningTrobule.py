cc09079b8fcf456f35f8f27f811b22f316f91235
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-06 19:09:25 +0100

    update tests

--- Day6/test_tuningTrobule.py
+++ Day6/test_tuningTrobule.py
@@ -6,6 +6,21 @@ def test_checkRepetition():
     assert checkRepetition("abcd") == False
 
 
+def test_solvePart1TestInput():
+    assert solve(['bvwbjplbgvbhsrlpgdmjqwftvncz'], 4) == 5
+    assert solve(['nppdvjthqldpwncqszvftbrmjlhg'], 4) == 6
+    assert solve(['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'], 4) == 10
+    assert solve(['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'], 4) == 11
+
+
+def test_solvePart2TestInput():
+    assert solve(['mjqjpqmgbljsphdztnvjfqwrcgsmlb'], 14) == 19
+    assert solve(['bvwbjplbgvbhsrlpgdmjqwftvncz'], 14) == 23
+    assert solve(['nppdvjthqldpwncqszvftbrmjlhg'], 14) == 23
+    assert solve(['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'], 14) == 29
+    assert solve(['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'], 14) == 26
+
+
 def test_solvePart1():
     assert solve(data, 4) == 1794
 
