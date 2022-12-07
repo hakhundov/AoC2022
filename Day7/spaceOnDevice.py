e895b7e8fadd3b0bd1a0265afcc35c5c72ace3f2
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-07 22:05:44 +0100

    d7 put all into one function

--- Day7/spaceOnDevice.py
+++ Day7/spaceOnDevice.py
@@ -9,51 +9,58 @@ data = get_data(day=7, year=2022).splitlines()
 #     for line in file:
 #         data.append(''.join(line.splitlines()))
 
-totalSum = 0
-stack = []
-stack.append(0)
-currentSize = 0
-dirSizes = []
 
-for line in data:
-    tokens = line.strip().split(' ')
-    match tokens[0]:
-        case '$':
-            match tokens[1]:
-                case 'cd':
-                    if (tokens[2] == '..'):  # up directory
-                        stack[-1] += currentSize
-                        currentSize = 0
-                        if (size := stack.pop()) <= 100000:
-                            totalSum += size
-                        dirSizes.append(size)  # added for part 2
-                        stack[-1] += size  # parent also inherits size
-                    else:  # down directory
-                        stack[-1] += currentSize
-                        currentSize = 0
-                        stack.append(0)
-                case 'ls':
-                    pass
-        case 'dir':
-            pass
-        case _:
-            currentSize += int(tokens[0])
-
-# unwind stack
-while (len(stack) > 1):
-    stack[-1] += currentSize
+def solve(data):
+    totalSum = 0
+    stack = []
+    stack.append(0)
     currentSize = 0
-    if (size := stack.pop()) <= 100000:
-        totalSum += size
-    dirSizes.append(size)  # added for part 2
-    stack[-1] += size  # parent also inherits size
+    dirSizes = []
 
-# part 1 answer
-print(totalSum)
+    for line in data:
+        tokens = line.strip().split(' ')
+        match tokens[0]:
+            case '$':
+                match tokens[1]:
+                    case 'cd':
+                        if (tokens[2] == '..'):  # up directory
+                            stack[-1] += currentSize
+                            currentSize = 0
+                            if (size := stack.pop()) <= 100000:
+                                totalSum += size
+                            dirSizes.append(size)  # added for part 2
+                            stack[-1] += size  # parent also inherits size
+                        else:  # down directory
+                            stack[-1] += currentSize
+                            currentSize = 0
+                            stack.append(0)
+                    case 'ls':
+                        pass
+            case 'dir':
+                pass
+            case _:
+                currentSize += int(tokens[0])
 
-totalSize = sum(stack) + currentSize
-unusedSpace = 70000000 - totalSize
-stillRequired = 30000000 - unusedSpace
-dirSizes.sort()
-# part 2 answer
-print([i for i in dirSizes if i > stillRequired][0])
+    # unwind stack
+    while (len(stack) > 1):
+        stack[-1] += currentSize
+        currentSize = 0
+        if (size := stack.pop()) <= 100000:
+            totalSum += size
+        dirSizes.append(size)  # added for part 2
+        stack[-1] += size  # parent also inherits size
+
+    # part 1 answer
+    # print(totalSum)
+
+    totalSize = sum(stack)
+    unusedSpace = 70000000 - totalSize
+    stillRequired = 30000000 - unusedSpace
+    dirSizes.sort()
+    # part 2 answer
+    spaceToFree = [i for i in dirSizes if i > stillRequired][0]
+    # print(spaceToFree)
+    return [totalSum, spaceToFree]
+
+
+print(solve(data))
