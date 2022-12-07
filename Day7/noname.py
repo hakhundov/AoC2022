f188cc4f623203bc019e05716a620171ac919c6a
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-07 20:40:19 +0100

    d7p1 passing part1 solved

--- Day7/noname.py
+++ Day7/noname.py
@@ -16,44 +16,44 @@ currentSize = 0
 
 collect = []
 
-with open("testInput", "r") as file:
-    for line in file:
-        tokens = line.strip().split(' ')
-        print(tokens)
-        match tokens[0]:
-            case '$':
-                match tokens[1]:
-                    case 'cd':
-                        if (tokens[2] == '..'):
-                            stack[-1] += currentSize
-                            print('Stack just before popping! -->' , stack)
-                            currentSize = 0
-                            if (size := stack.pop()) <= 100000:
-                                print ('inside if statment, size = ', size)
-                                totalSum += size
-                                collect.append(size)
-                            stack[-1] += size # parent also inherits size
-                            #do things with stack, size and current max
-                        else: #this means we are entering a new dir
-                            print('entering a new dir ' + tokens [2])
-                            stack[-1] += currentSize
-                            currentSize = 0
-                            stack.append(0)
-                            
-                    case 'ls':
-                        pass #safely ignore this block
-            case 'dir':
-                pass #safely ignore this block
+# with open("testInput", "r") as file:
+for line in data:
+    tokens = line.strip().split(' ')
+    print(tokens)
+    match tokens[0]:
+        case '$':
+            match tokens[1]:
+                case 'cd':
+                    if (tokens[2] == '..'):
+                        stack[-1] += currentSize
+                        print('Stack just before popping! -->' , stack)
+                        currentSize = 0
+                        if (size := stack.pop()) <= 100000:
+                            print ('inside if statment, size = ', size)
+                            totalSum += size
+                            collect.append(size)
+                        stack[-1] += size # parent also inherits size
+                        #do things with stack, size and current max
+                    else: #this means we are entering a new dir
+                        print('entering a new dir ' + tokens [2])
+                        stack[-1] += currentSize
+                        currentSize = 0
+                        stack.append(0)
+                        
+                case 'ls':
+                    pass #safely ignore this block
+        case 'dir':
+            pass #safely ignore this block
 
-            case _: #let's calculate the size!
-                currentSize += int(tokens[0])
+        case _: #let's calculate the size!
+            currentSize += int(tokens[0])
 
-        # print('Current state')
-        print('Stack = ' ,stack)
-        print('Current size = ', currentSize)
-        print('total sum =', totalSum)
-        print('collect --> ', collect)
-        print(' ')
+    # print('Current state')
+    print('Stack = ' ,stack)
+    print('Current size = ', currentSize)
+    print('total sum =', totalSum)
+    print('collect --> ', collect)
+    print(' ')
 print(totalSum)
 
 # print(data)
