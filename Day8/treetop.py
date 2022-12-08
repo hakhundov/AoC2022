dd3550ce63fdcd5a1e6909ed07c52ed2c10c24e7
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-08 23:57:22 +0100

    D8 clean

--- Day8/treetop.py
+++ Day8/treetop.py
@@ -6,76 +6,61 @@ data = get_data(day=8, year=2022).splitlines()
 testData = []
 with open("testInput", "r") as file:
     for line in file:
-        testData.append([*''.join(line.splitlines())]) #unpack
+        testData.append([*''.join(line.splitlines())])  # unpack
 
 
-print(testData)
-# print(len(data))
-
-    #naive implementation
+# naive implementation
 def solve(data):
-    count = 0
     width = len(data[0])
     length = len(data)
-
-    # print(width, length)
     visibleEdges = 2 * width + 2 * length - 4
-    # print(visibleEdges)
+    flag = [[0]*width for i in range(length)]
 
-    flag = [ [0]*width for i in range(length)]
-    # print(flag)
-
-    maxT = 0
-    maxL = 0
+    maxHeight = 0
+    maxHeight = 0
 
     # check from left
     for i in range(1, width-1, 1):
-        maxL = 0
-        for j in range (1, length-1, 1):
-            if (int(data[i][j]) > int(data[i][j-1])) and (int(data[i][j]) > maxL):
+        maxHeight = 0
+        for j in range(1, length-1, 1):
+            if (int(data[i][j]) > int(data[i][j-1])) and (int(data[i][j]) > maxHeight):
                 flag[i][j] = 1
-                maxL = max(maxL, int(data[i][j]))
+                maxHeight = max(maxHeight, int(data[i][j]))
             else:
-                 maxL = max(maxL, int(data[i][j-1]))
-    # print(flag)
+                maxHeight = max(maxHeight, int(data[i][j-1]))
 
     # check from top
     for j in range(1, width-1, 1):
-        maxT = 0
-        for i in range (1, length-1, 1):
-            # print("i, j =", i, j)
-            if (int(data[i][j]) > int(data[i-1][j])) and (int(data[i][j]) > maxT):
+        maxHeight = 0
+        for i in range(1, length-1, 1):
+            if (int(data[i][j]) > int(data[i-1][j])) and (int(data[i][j]) > maxHeight):
                 flag[i][j] = 1
-                maxT = max(maxT, int(data[i][j]))
+                maxHeight = max(maxHeight, int(data[i][j]))
             else:
-                maxT = max(maxT, int(data[i-1][j]))
-    # print(flag)
+                maxHeight = max(maxHeight, int(data[i-1][j]))
 
     # check from right
     for i in range(width-2, 0, -1):
-        maxT = 0
-        for j in range (length-2, 0, -1):
-            # print("i, j =", i, j)
-            if (int(data[i][j]) > int(data[i][j+1])) and (int(data[i][j]) > maxT):
+        maxHeight = 0
+        for j in range(length-2, 0, -1):
+            if (int(data[i][j]) > int(data[i][j+1])) and (int(data[i][j]) > maxHeight):
                 flag[i][j] = 1
-                maxT = max(maxT, int(data[i][j]))
+                maxHeight = max(maxHeight, int(data[i][j]))
             else:
-                maxT = max(maxT, int(data[i][j+1]))
-    # print(flag)
+                maxHeight = max(maxHeight, int(data[i][j+1]))
 
     # check from bottom
     for j in range(width-2, 0, -1):
-        maxT = 0
-        for i in range (length-2, 0, -1):
-            print("i, j =", i, j)
-            if (int(data[i][j]) > int(data[i+1][j])) and (int(data[i][j]) > maxT):
+        maxHeight = 0
+        for i in range(length-2, 0, -1):
+            if (int(data[i][j]) > int(data[i+1][j])) and (int(data[i][j]) > maxHeight):
                 flag[i][j] = 1
-                maxT = max(maxT, int(data[i+1][j]))
+                maxHeight = max(maxHeight, int(data[i+1][j]))
             else:
-                maxT = max(maxT, int(data[i+1][j]))
-    # print(flag)
+                maxHeight = max(maxHeight, int(data[i+1][j]))
 
-    return sum(sum(flag,[])) + visibleEdges # TODO: understand how sum flattens the list
+    # TODO: understand how sum flattens the list
+    return sum(sum(flag, [])) + visibleEdges
+
 
 print(solve(data))
-
