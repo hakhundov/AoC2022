1a543a0f2d10ab0ad2c1be5337dc896e655cbe5a
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-12 11:30:15 +0100

    day 12: add basic functions canMove, getNeighbors, findStart

--- Day12/day12.py
+++ Day12/day12.py
@@ -3,6 +3,7 @@
 from aocd import get_data
 import re
 import math
+import queue
 
 data = get_data(day=12, year=2022).splitlines()
 testData = []
@@ -10,3 +11,34 @@ with open("testInput", "r") as file:
     for line in file:
         testData.append(line.strip())  # unpack
 
+row_len = len(data)
+col_len = len(data[0])
+print(row_len, col_len)
+
+def canMove(fr, to):
+    if (ord(to) - ord(fr)) > 1:
+        return False
+    else:
+        return True
+
+
+def getNeighbors(row, col, row_len, col_len):
+    neighbors = []
+    if row > 0:
+        above = (row-1, col)
+        neighbors.append(above)
+    if row < row_len - 1:
+        below = (row+1, col)
+        neighbors.append(below)
+    if col > 0:
+        left = (row, col-1)
+        neighbors.append(left)
+    if col < col_len - 1:
+        right = (row, col+1)
+        neighbors.append(right)
+    return neighbors
+
+def findStart(data):
+    for i in range(len(data)):
+        if (j := data[i].find('S')) != -1:
+            return (i, j)
