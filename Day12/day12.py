974827965998db22a4b3dc016af0e6144faba539
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-12 12:21:03 +0100

    day 12: breadth first search sceleton

--- Day12/day12.py
+++ Day12/day12.py
@@ -38,7 +38,7 @@ def getNeighbors(data, row, col, row_len, col_len):
     if col < col_len - 1:
         right = (row, col+1)
         allNeighbors.append(right)
-    
+
     filtered = [n for n in allNeighbors if canMove(data[row][col], data[n[0]][n[1]])]
     return filtered
 
@@ -46,3 +46,28 @@ def findStart(data):
     for i in range(len(data)):
         if (j := data[i].find('S')) != -1:
             return (i, j)
+
+
+start = findStart(data)
+print(start)
+
+frontier = queue.Queue()
+# reached = []
+frontier.put(start)
+# reached.append(start)
+came_from = dict()
+came_from[start] = None
+
+# count = 0
+while not frontier.empty():
+    current = frontier.get()
+    # count += 1
+    for next in getNeighbors(current[0], current[1]):
+        if next not in came_from:
+            frontier.put(next)
+            came_from[next] = current
+            # reached.append(next)
+# implement early exit
+
+
+print(came_from)
