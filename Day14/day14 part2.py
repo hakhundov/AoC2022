0db9d99f9723abf40054e452f26182cefeaf6d32
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-14 23:03:15 +0100

    day 14: part 1 and 2 solved

--- Day14/day14 part2.py
+++ Day14/day14 part2.py
@@ -35,12 +35,21 @@ for line in scanData:
             max_y = coordinate[1]
 print(min_x, max_x, min_y, max_y)
 
-cave = [ ['.'] * (max_x-min_x+1) for i in range(max_y-min_y+1)]
+offset = min_x + max_y
+# min_x -= max_y #conservative
+# max_x += max_y
+
+cave = [['.'] * (max_x - min_x + 2 * max_y + 1) for i in range(max_y-min_y+1)]
+extendCave = [['.'] * (max_x - min_x + 2 * max_y + 1)]
+extendCave.extend([['#'] * (max_x - min_x + 2 * max_y + 1)])
+cave.extend(extendCave)
+
+# pprint(cave)
 
 for line in scanData:
     for i in range(len(line)-1):
-        pointA = (line[i][0]-min_x, line[i][1])
-        pointB = (line[i+1][0]-min_x, line[i+1][1])
+        pointA = (line[i][0]-offset, line[i][1])
+        pointB = (line[i+1][0]-offset, line[i+1][1])
 
         k1 = 1 if pointB[0] - pointA[0] >= 0 else -1
         for x in range(pointA[0], pointB[0]+k1, k1):
@@ -48,38 +57,46 @@ for line in scanData:
             for y in range(pointA[1], pointB[1]+k2, k2):
                 cave[y][x] = '#'
 
-sandStart = 500 - min_x
+
+sandStart = 500 - offset
 atRestCount = 0
 
-# abyss = False
-# while not abyss:
-while True:
-    sandPosition = [0,sandStart]
+blocking = False
+while not blocking:
+    # print(blocking)
+    sandPosition = [0, sandStart]
     atRest = False
     try:
         while not atRest:
             if sandPosition[1] > max_y:
                 abyss = True
                 break
-            if cave[ sandPosition[0]+1 ][ sandPosition[1] ] == '.':
+            if cave[sandPosition[0]+1][sandPosition[1]] == '.':
                 sandPosition[0] += 1
                 continue
-            elif cave[ sandPosition[0]+1 ][ sandPosition[1] ] != '.': # i.e., # or o
+            elif cave[sandPosition[0]+1][sandPosition[1]] != '.':  # i.e., # or o
                 # attempt left
-                if cave[ sandPosition[0]+1 ][ sandPosition[1]-1 ] == '.':
+                if cave[sandPosition[0]+1][sandPosition[1]-1] == '.':
                     sandPosition[0] += 1
                     sandPosition[1] -= 1
                     continue
                 # attempt right
-                elif cave[ sandPosition[0]+1 ][ sandPosition[1]+1 ] == '.':
+                elif cave[sandPosition[0]+1][sandPosition[1]+1] == '.':
                     sandPosition[0] += 1
                     sandPosition[1] += 1
                     continue
-                else: # rest
+                else:  # rest
                     atRest = True
                     atRestCount += 1
-                    cave[ sandPosition[0] ][ sandPosition[1] ] = 'o'
+                    cave[sandPosition[0]][sandPosition[1]] = 'o'
+                    if (sandPosition[0] == 0) and (sandPosition[1] == sandStart):
+                        blocking = True
+                        break
+                # pprint(cave)
     except:
-        break    
+        break
+    # print(atRestCount)
 
 print(atRestCount)
+# pprint(cave)
+#correct answer 27539
