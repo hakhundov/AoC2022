5eb94ebc5c0a2bbf7ff7bcd4962db00a83fff12e
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-14 23:04:17 +0100

    removed obfuscated monkey

--- Day14/day14.py
+++ Day14/day14.py
@@ -5,12 +5,12 @@ import copy
 from functools import cmp_to_key
 from pprint import pprint
 
-data = get_data(day=14, year=2022).splitlines()
-# data = []
-# with open("testInput", "r") as file:
-#     for line in file:
-#         data.append(line.strip())  # unpack
-# print(data)
+# data = get_data(day=14, year=2022).splitlines()
+data = []
+with open("testInput", "r") as file:
+    for line in file:
+        data.append(line.strip())  # unpack
+print(data)
 
 scanData = []
 
