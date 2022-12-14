95c1850588655c0769f6eebd35763316ee6bc40a
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-14 07:47:48 +0100

    day 14: add empty files

---
+++ Day14/day14.py
@@ -0,0 +1,12 @@
+# AoC 2022 - Day 14 - https://adventofcode.com/2022/day/14
+
+from aocd import get_data
+import copy
+from functools import cmp_to_key
+
+
+data = get_data(day=14, year=2022).splitlines()
+# data = []
+# with open("testInput", "r") as file:
+#     for line in file:
+#         data.append(line.strip())  # unpack
