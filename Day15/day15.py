2cf7fb01684dbcc9e675c849b4beb3582ff71ec6
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-15 10:34:13 +0100

    Day 15: works for testInput, but too slow for real data

--- Day15/day15.py
+++ Day15/day15.py
@@ -4,11 +4,36 @@ from aocd import get_data
 import copy
 from functools import cmp_to_key
 from pprint import pprint
+from pixelscan import manhattan, ringscan
+import re
+import multiprocessing
 
-# data = get_data(day=15, year=2022).splitlines()
+data = get_data(day=15, year=2022).splitlines()
 
-data = []
-with open("testInput", "r") as file:
-    for line in file:
-        data.append(line.strip())
-print(data)
+# data = []
+# with open("testInput", "r") as file:
+#     for line in file:
+#         data.append(line.strip())
+
+beaconNotPresent = set()
+beacons = set()
+for line in data:
+    print(line)
+    matches = [int(n) for n in re.findall(r"-?\d+", line)]
+    sensor = [matches[0], matches[1]]
+    beacon = [matches[2], matches[3]]
+    beacons.add((matches[2], matches[3]))
+
+    d = manhattan(sensor, beacon)
+    # print(d)
+    for x, y in ringscan(matches[0], matches[1], 0, d, metric=manhattan):
+        beaconNotPresent.add((x,y))
+
+beaconNotPresent -= beacons
+count = 0
+y = 2000000
+for point in beaconNotPresent:
+    if point[1] == y:
+        print(point)
+        count += 1
+print(count)
