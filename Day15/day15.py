44ac1887bf40bbfc6265e4670372a38be963409a
Haji Akhundov <hajiakhundov@gmail.com>
2022-12-17 09:37:08 +0100

    day 15: refactor

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,9 +1,7 @@
 # AoC 2022 - Day 15 - https://adventofcode.com/2022/day/15
 
 from aocd import get_data
-from functools import cmp_to_key
-from pprint import pprint
-from pixelscan import manhattan, ringscan
+# from pixelscan import manhattan, ringscan
 import re
 
 data = get_data(day=15, year=2022).splitlines()
@@ -13,29 +11,33 @@ with open("testInput", "r") as file:
     for line in file:
         testData.append(line.strip())
 
+
+def manhattan(point1, point2):
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
 def processSensorBeaconData(data):
     beacons = set()
-    sbdata = []
+    sensorData = []
     for line in data:
         matches = [int(n) for n in re.findall(r"-?\d+", line)]
         sensor = [matches[0], matches[1]]
         beacon = [matches[2], matches[3]]
         beacons.add((matches[2], matches[3]))
-        d = manhattan(sensor, beacon)
-        sbdata.append([sensor, beacon, d])
+        distance = manhattan(sensor, beacon)
+        sensorData.append([sensor, beacon, distance])
 
-    leftMostReach = min([x[0][0] - x[2] for x in sbdata])
-    rightMostReach = max([x[0][0] + x[2] for x in sbdata])
-    topMostReach = min([x[0][1] - x[2] for x in sbdata])
-    bottomMostReach = max([x[0][1] + x[2] for x in sbdata])
-    return beacons,sbdata,leftMostReach,rightMostReach,topMostReach,bottomMostReach
+    leftMostReach = min([x[0][0] - x[2] for x in sensorData])
+    rightMostReach = max([x[0][0] + x[2] for x in sensorData])
+    topMostReach = min([x[0][1] - x[2] for x in sensorData])
+    bottomMostReach = max([x[0][1] + x[2] for x in sensorData])
+    return beacons, sensorData, leftMostReach, rightMostReach, topMostReach, bottomMostReach
 
 
-def countNoBeacons(beacons, sbdata, leftMostReach, rightMostReach, y):
+def countNoBeacons(beacons, sensorData, leftMostReach, rightMostReach, y):
     beaconNotPresent = set()
     for x in range(leftMostReach, rightMostReach+1, 1):
         point = [x, y]
-        for s in sbdata:
+        for s in sensorData:
             if manhattan(point, s[0]) <= s[2]:
                 beaconNotPresent.add((point[0], point[1]))
                 break
@@ -43,9 +45,36 @@ def countNoBeacons(beacons, sbdata, leftMostReach, rightMostReach, y):
 
 
 def solve(data, y):
-    beacons, sbdata, leftMostReach, rightMostReach, _, _ = processSensorBeaconData(data)
-    print(countNoBeacons(beacons, sbdata, leftMostReach, rightMostReach, y))
+    beacons, sensorData, leftMostReach, rightMostReach, _, _ = processSensorBeaconData(
+        data)
+    print(countNoBeacons(beacons, sensorData, leftMostReach, rightMostReach, y))
 
-solve(data, 2000000)
-solve(testData, 10)
-#4424278
+# solve(data, 2000000)
+# solve(testData, 10)
+# part 1 4424278
+# part 2 10382630753392
+
+
+def getRanges(sensorData, row):
+    rowRanges = []
+    for sensor in sensorData:
+        d = sensor[2] - abs(row - sensor[0][1])
+        if d >= 0:  # there is overlap
+            rowRanges.append((sensor[0][0] - d, sensor[0][0] + d))
+    return sorted(rowRanges)
+
+_, sensorData, _, _, _, _ = processSensorBeaconData(data)
+# print(getRanges(sensorData, 10))
+
+def findGapInRanges(ranges):
+    highest = 0
+    for (a, b) in ranges:
+        if a <= highest+1:
+            highest = max(b, highest)
+        else:
+            return a-1
+
+for row in range (4000000):
+    ranges = getRanges(sensorData, row)
+    if index := findGapInRanges(ranges):
+        print(index*4000000 + row)
